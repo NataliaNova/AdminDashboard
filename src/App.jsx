26da843de92ea94b61c60f19b0ae3a4fad962130
NatNov <100388491+NataliaNova@users.noreply.github.com>
2025-05-31 09:21:25 +0200

    Agrego vercel.json y config final

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,3 @@
-/* import { Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import UserTable from './components/UserTable';
-import Login from './pages/Login';
-import PrivateRoute from './components/PrivateRoute';
-import Layout from './components/Layout';
-
-
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <Layout />
-          </PrivateRoute>
-        }
-      >
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="usuarios" element={<UserTable />} />
-      </Route>
-    </Routes>
-  );
-}
-
-export default App;
- */
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import UserTable from './components/UserTable';
@@ -41,8 +10,6 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-
-      {/* Rutas protegidas bajo Layout */}
       <Route
         path="/"
         element={
